7160ec2b58532108669999839ce436253d5dcc8c
Halaylanderson Mendes da Silva <haaley_watch@hotmail.com>
2019-03-15 23:20:36 -0300

    initial commit

---
+++ Routes/public.js
@@ -0,0 +1,41 @@
+const express = require("express");
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const router = express.Router();
+
+// Home page
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.post('/', function (req, res) {
+    let mailOpts, smtpTrans;
+    smtpTrans = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      }
+    });
+    mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+      to: 'halaylanderson2009@gmail.com',
+      subject: 'Nova Mensagem de Upaon Solutions',
+      text: `${req.body.name} telefone: ${req.body.phone} (${req.body.email}) mandou a mensagem: ${req.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts, function (error, response) {
+      if (error) {
+            console.log(error);
+      }
+      else {
+            console.log(error);
+            res.render('index');
+      }
+    });
+  });
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser= require('body-parser');
+require('dotenv').config()
+
+
+engine = require('ejs-locals');
+const publicRouter = require("./routes/public");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.engine('ejs', engine);
+
+app.set('views',__dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs'); 
+
+app.use('/', publicRouter);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+console.log(process.env.GMAIL_USER);
+console.log(process.env.GMAIL_PASS);
